f5eb5dc166454915149363602e91c74f897892c2
tkachevdg <tkachevdg@gmail.com>
2022-12-02 01:42:59 +0600

    Приветсвие Маши

---
+++ exam5_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя:");
+string username = Console.ReadLine();
+
+if(username.ToLower()=="маша")
+{
+    Console.WriteLine("Здравствуй, Машенька!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
